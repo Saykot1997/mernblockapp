e8e4268142e9dcf35e77707404f6fa974dbd06b5
Saykot Hossain <88452518+Saykot1997@users.noreply.github.com>
2022-11-19 22:50:12 +0600

    Update Data.js
    live api link add

--- client/src/Data.js
+++ client/src/Data.js
@@ -1,2 +1,3 @@
-export const Host = "http://localhost:5000/api";
+// export const Host = "http://localhost:5000/api";
 // export const Host = "http://saykotblog.io/api";
+export const Host = "https://mernblogap.herokuapp.com/api";
